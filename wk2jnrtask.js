b1b640e2152020b527f2724267f2f50f92e62a28
Muhammad Usman <usmankh606@gmail.com>
2026-09-15 06:49:27 +0500

    Initial commit

---
+++ wk2jnrtask.js
@@ -0,0 +1,13 @@
+let arr=[
+    {Day:'monday',expence:200},
+    {Day:'tuesday',expence:220},
+    {Day:'wednesday',expence:250},
+    {Day:'thursday',expence:280},
+    {Day:'friday',expence:290},
+    {Day:'saturday',expence:300},
+    {Day:'sunday',expence:310}
+]
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
